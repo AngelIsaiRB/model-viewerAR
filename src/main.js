a5cc1b8b6e9210583f82cb33892ecf83312408d1
Anegelisai <63125934+Anegelisai@users.noreply.github.com>
2021-02-11 14:14:43 -0600

    load model button

--- src/main.js
+++ src/main.js
@@ -5,3 +5,12 @@
 // window.addEventListener('resize', () => {
 // 	app.onResize();
 // });
+document.querySelector("#model-viewer").addEventListener('ar-status', (event) => {
+    if(event.detail.status === 'failed'){
+        const error = document.querySelector("#error");
+        error.classList.remove('hide');
+        error.addEventListener('transitionend',(event) => {
+            error.classList.add('hide');
+        });
+    }
+});
