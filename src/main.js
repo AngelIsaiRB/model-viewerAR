834ee38457e75beb8333e1f0da09cba6189bced2
Anegelisai <63125934+Anegelisai@users.noreply.github.com>
2021-02-11 17:18:04 -0600

    multiple onoad models

--- src/main.js
+++ src/main.js
@@ -5,6 +5,12 @@
 // window.addEventListener('resize', () => {
 // 	app.onResize();
 // });
+const models = ['botella/scene.gltf', 'cube/cube.gltf', 'torus/torus.gltf'];
+const posters = ["url('./assets/Arte 3D/botella/img/botella.png')", "url('./assets/Arte 3D/cube/cube.png')","url('./assets/Arte 3D/torus/torus.png')"]
+let count=0;
+const toggleModel = document.querySelector('#model-viewer');
+
+
 document.querySelector("#model-viewer").addEventListener('ar-status', (event) => {
     if(event.detail.status === 'failed'){
         const error = document.querySelector("#error");
@@ -14,3 +20,20 @@ document.querySelector("#model-viewer").addEventListener('ar-status', (event) =>
         });
     }
 });
+
+document.querySelector("#next").onclick = () =>{
+    count++;
+    if(count >= 3 ) count = 0;
+    document.querySelector("#lazy-load-poster").style.backgroundImage = posters[count]
+    toggleModel.setAttribute('src', `./assets/Arte 3D/${models[count]}`)
+}
+document.querySelector("#back").onclick = () =>{
+    count -= 1 ;
+    if(count <= -1 ) count = 2;
+    document.querySelector("#lazy-load-poster").style.backgroundImage = posters[count]
+    toggleModel.setAttribute('src', `./assets/Arte 3D/${models[count]}`)
+}
+
+// const toggleModel = document.querySelector('#model-viewer');
+// let j = 0;
+// setInterval(() => toggleModel.setAttribute('src', `./assets/Arte 3D/${models[j++ % 2]}`), 2000);
